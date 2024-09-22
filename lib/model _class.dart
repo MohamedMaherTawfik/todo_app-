3237a4dc11ee49289f2dfd09b7b6e7e4f40c0a41
Mohamed Maher <141060515+MohamedMaherTawfik@users.noreply.github.com>
2024-09-23 00:11:12 +0300

    initializing hive database

--- lib/model _class.dart
+++ lib/model _class.dart
@@ -1,15 +1,15 @@
-// import 'package:hive/hive.dart';
+import 'package:hive/hive.dart';
 
-// part 'model _class.g.dart';
+part 'model _class.g.dart';
 
-// @HiveType(typeId: 0)
-// class Model_Class {
-//   @HiveField(0)
-//   final String name;
-//   @HiveField(1)
-//   final int age;
-//   @HiveField(2)
-//   final int phone;
+@HiveType(typeId: 0)
+class Model_Class {
+  @HiveField(0)
+  final String name;
+  @HiveField(1)
+  final int age;
+  @HiveField(2)
+  final int phone;
 
-//   Model_Class({required this.name, required this.age, required this.phone});
-// }
+  Model_Class({required this.name, required this.age, required this.phone});
+}
